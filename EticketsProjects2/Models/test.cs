58886da7c1e58092547cef07f3f5687c5927793c
JithanRoy <jonyyy81@gmail.com>
2022-01-04 18:00:15 +0600

    class added

---
+++ EticketsProjects2/Models/test.cs
@@ -0,0 +1,9 @@
+﻿namespace EticketsProjects2.Models
+{
+    public class test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
