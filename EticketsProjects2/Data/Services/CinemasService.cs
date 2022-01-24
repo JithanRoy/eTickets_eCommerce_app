9097d992c6c765369682fbbb379fbfdba8c8e76e
jitenroyjony <jonyyy81@gmail.com>
2022-01-24 14:12:45 +0600

    cinemas updated

---
+++ EticketsProjects2/Data/Services/CinemasService.cs
@@ -0,0 +1,14 @@
+﻿using eTickets.Data;
+using eTickets.Models;
+using EticketsProjects2.Data.Base;
+
+namespace EticketsProjects2.Data.Services
+{
+    public class CinemasService : EntityBaseRepository<Cinema>, ICinemasService
+    {
+        public CinemasService(AppDbContext context): base(context)
+        {
+
+        }
+    }
+}
---
+++ EticketsProjects2/Data/Services/ICinemasService.cs
@@ -0,0 +1,10 @@
+﻿using eTickets.Models;
+using EticketsProjects2.Data.Base;
+
+namespace EticketsProjects2.Data.Services
+{
+    public interface ICinemasService : IEntityBaseRepository<Cinema>
+    {
+        
+    }
+}
